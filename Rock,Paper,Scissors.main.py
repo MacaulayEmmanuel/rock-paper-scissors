290510317d459bd5153ade5f3e509c915c5704f8
MacaulayEmmanuel <105212075+MacaulayEmmanuel@users.noreply.github.com>
2022-06-01 12:07:57 +0100

    Add files via upload

---
+++ Rock,Paper,Scissors.main.py
@@ -0,0 +1,52 @@
+import random
+
+while True:
+    choices = ["r","p","s"]
+
+    computer = random.choice(choices)
+    player = None
+
+    while player not in choices:
+        player = input("r, p, or s?: ").lower()
+
+    if player == computer:
+        print("computer: ",computer)
+        print("player: ",player)
+        print("Tie!")
+
+    elif player == "r":
+        if computer == "p":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You lose!")
+        if computer == "s":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You win!")
+
+    elif player == "s":
+        if computer == "r":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You lose!")
+        if computer == "p":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You win!")
+
+    elif player == "p":
+        if computer == "s":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You lose!")
+        if computer == "r":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You win!")
+
+    play_again = input("Play again? (yes/no): ").lower()
+
+    if play_again != "yes":
+        break
+
+print("See you later, o da bo!")
